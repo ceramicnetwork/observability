2a672b9d5cd9f1b3b7eb3212a91c7ada207dedad
Golda Velez <golda@3box.io>
2023-12-22 20:45:03 -0700

    add the package files

---
+++ packages/model-metrics/src/createModels.ts
@@ -0,0 +1,22 @@
+import type { CeramicApi } from '@ceramicnetwork/common'
+import { Composite } from '@composedb/devtools'
+
+import { metricSchema } from './simpleNodeMetrics'
+
+declare global {
+  const ceramic: CeramicApi
+}
+
+async function createComposite() {
+  try {
+    const composite = await Composite.create({ ceramic, schema: metricSchema });
+    console.log(composite);
+    debugger;
+    return composite;
+  } catch (error) {
+    console.error('Error creating composite:', error);
+  }
+}
+
+// Call the async function
+createComposite();
