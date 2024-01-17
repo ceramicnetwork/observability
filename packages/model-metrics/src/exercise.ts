d563a1b2200191d56efe4eb9bef574f979fa00e0
Golda Velez <golda@3box.io>
2024-01-17 03:13:20 -0700

    resolve dependencies

--- packages/model-metrics/src/exercise.ts
+++ packages/model-metrics/src/exercise.ts
@@ -1,10 +1,15 @@
-import { CeramicClient } from "@ceramicnetwork/http-client";
+import { CeramicClient } from "@ceramicnetwork/http-client"
 import { createMetricComposite} from './createModels.js'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { DID } from 'dids'
+import { getResolver } from 'key-did-resolver'
+import { fromString } from 'uint8arrays'
+import {readFileSync} from 'fs'
 
 // Not used in production - the ceramic object would be passed 
 const CERAMIC_URL = process.env.CERAMIC_URL;
 const ceramic = new CeramicClient(CERAMIC_URL);
-/*
+
 const authenticate = async () => {
   const seed = readFileSync("./admin_seed.txt", 'utf8').trim();
   const key = fromString(seed, "base16");
@@ -18,7 +23,7 @@ const authenticate = async () => {
 };
 //const did = await authenticate()
 //compose.setDID(did)
-*/
+
 
 // Call the async function
 createMetricComposite(ceramic);
